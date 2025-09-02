f40b349049e2404fcc816173837120c851cb5a70
gatlingmcgee <rzman03@aol.com>
2025-09-02 16:55:46 -0500

    updated meta tags

--- week01/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// copyright year
-document.querySelector('#currentyear').textContent = new Date().getFullYear();
-
-// last modified date
-document.querySelector('#lastmodified').textContent = ` Last Modified: ${document.lastModified}`;
-
