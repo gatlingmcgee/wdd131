e647b8d1b340a99b410aab52aac5d4f557ebe4ac
gatlingmcgee <rzman03@aol.com>
2025-09-08 15:00:19 -0500

    folder structure for W02 Assignment: Picture Album

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+// copyright year
+document.querySelector('#currentyear').textContent = new Date().getFullYear();
+
+// last modified date
+document.querySelector('#lastmodified').textContent = ` Last Modified: ${document.lastModified}`;
+
