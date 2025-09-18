7acf031f6ecf8189b9e605ee7ddb92f798479060
gatlingmcgee <rzman03@aol.com>
2025-09-18 14:18:33 -0500

    added js

--- scripts/indexWeather.js
+++ scripts/indexWeather.js
@@ -1,44 +1,18 @@
-const state = 'Oklahoma, USA';
-document.getElementById('state-placeholder').textContent = state;
+const temperatureF = 45;
+const windSpeedMph = 5;
 
-const myTown = document.querySelector('#town');
-const myDescription = document.querySelector('#description');
-const myTemperature = document.querySelector('#temperature');
-const myGraphic = document.querySelector('#graphic');
-
-
-const url = '//api.openweathermap.org/data/2.5/weather?lat=36.06&lon=-95.80&appid=25baae6a81058afccd7517c966ed3aed&units=imperial';
-
-async function apiFetch() {
-	try {
-		const response = await fetch(url);
-		if (response.ok) {
-			const data = await response.json();
-			console.log(data); //for testing
-			displayResults(data);
-		} else {
-			throw Error (await response.text());
-		}
-	} catch (error) {
-		console.log(error);
-	}
+function calculateWindChill(tempF, speedMph) {
+  return (
+    35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(speedMph, 0.16) +
+    0.4275 * tempF * Math.pow(speedMph, 0.16)
+  );
 }
 
-function displayResults(data) {
-	//console.log('hello')
-	myTown.innerHTML = data.name
-	myDescription.innerHTML = data.weather[0].description;
-	myTemperature.innerHTML = `${data.main.temp}&deg;F`
-	const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-	myGraphic.setAttribute('src', iconsrc)
-	myGraphic.setAttribute('alt', data.weather[0].description)
+if (temperatureF <= 50 && windSpeedMph > 3) {
+  const windChill = calculateWindChill(temperatureF, windSpeedMph).toFixed(1);
+  document.getElementById("windchill").textContent = `${windChill} °F`;
+} else {
+  document.getElementById("windchill").textContent = "N/A";
 }
-
-apiFetch();
-
-function goBack() {
-	window.history.back();
-}
-
-
-
